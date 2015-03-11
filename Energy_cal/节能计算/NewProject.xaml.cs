e5bffa73531dd83243e7c7e550e174575505d512
klfzlyt <327442095@qq.com>
2015-03-11 23:04:12 +0800

    修改了 NewProject的逻辑， 但是界面还没改

--- Energy_cal/节能计算/NewProject.xaml.cs
+++ Energy_cal/节能计算/NewProject.xaml.cs
@@ -23,11 +23,21 @@ namespace 节能计算
     /// </summary>
     public partial class NewProject : MetroWindow
     {
+        private string _selectedpath = "";
+        private string _projectname = "";
         private string pjtxmllocation=null;
+        private XmlDocument _configdoc = new XmlDocument();
         public NewProject()
         {
             InitializeComponent();
-            this.Closing += NewProject_Closing;
+          
+            _configdoc.Load("./Config.xml");
+            var ConfigPathSelectedelement = _configdoc.SelectSingleNode("Config/ConfigItem[1]") as XmlElement;
+            _selectedpath = ConfigPathSelectedelement.Attributes["Value"].Value;
+            var ConfigProNameelement = _configdoc.SelectSingleNode("Config/ConfigItem[2]") as XmlElement;
+            _projectname = ConfigProNameelement.Attributes["Value"].Value;
+            this.proname.Text = _projectname;
+            this.prolocation.Text = _selectedpath;
             this.AddHandler(Keyboard.KeyDownEvent,new  System.Windows.Input.KeyEventHandler(hanlderkeydown));                                                                  
         }
         private void hanlderkeydown(object sender, System.Windows.Input.KeyEventArgs e)
@@ -37,17 +47,60 @@ namespace 节能计算
                 Debug.WriteLine("您按下了ESC");
                 this.Close();
             }
-
         }
 
-
-        void NewProject_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        /// <summary>
+        /// 这个方法只负责对话框打开，  并 this.prolocation.Text = target;
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Another_Click(object sender, RoutedEventArgs e)
         {
-         
+            //这个应该只是用来给_selectedpath赋值的
+            System.Windows.Input.Mouse.OverrideCursor = System.Windows.Input.Cursors.Wait;
+          //  string location = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+      //      string original = GlobalResource.Templatepath;
+            System.Windows.Forms.FolderBrowserDialog dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.Description = "请选择项目文件路径";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string target = dialog.SelectedPath;
+              
+                // System.Windows.Forms.MessageBox.Show("已选择目标文件夹:" + target, "选择文件夹提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (string.IsNullOrEmpty(target))
+                {
+                  //  System.Windows.Forms.MessageBox.Show("请输入项目名称", "选择文件夹提示", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Information);
+                    System.Windows.Input.Mouse.OverrideCursor = null;
+                    return;
+                }
+                this.prolocation.Text = target;
+                //var targetstr = target + "\\" + this.proname.Text;
+                //if (!Directory.Exists(targetstr))
+                //{
+                //    // Create the directory it does not exist.
+                //    Directory.CreateDirectory(targetstr);
+                //    CopyDirectoryAndFiles(targetstr, new DirectoryInfo(location + original));
+                //}
+                //else
+                //{
+                //    Xceed.Wpf.Toolkit.MessageBox.Show("项目名已存在，请重新创建！");
+                //    Mouse.OverrideCursor = null;
+                //    return;
+                //}
+                //pjtxmllocation = targetstr + "\\" + "Pjt.xml";
+                //XmlDocument doc = new XmlDocument();
+                ////targetstr = this.prolocation.Text;
+                //doc.Load(targetstr + "\\" + "Pjt.xml");
+                //var FinalitemsTemplateelement = doc.SelectSingleNode("Files/File[1]") as XmlElement;
+                //var DevicesDBelement = doc.SelectSingleNode("Files/File[2]") as XmlElement;
+                //var DevicesCategoryelement = doc.SelectSingleNode("Files/File[3]") as XmlElement;
+                //FinalitemsTemplateelement.SetAttribute("Location", targetstr + "\\" + "FinalitemsTemplate.xml");
+                //DevicesDBelement.SetAttribute("Location", targetstr + "\\" + "DevicesDB.xml");
+                //DevicesCategoryelement.SetAttribute("Location", targetstr + "\\" + "DevicesCategory.xml");
+                //doc.Save(targetstr + "\\" + "Pjt.xml");
+            }
+            System.Windows.Input.Mouse.OverrideCursor = null;   
         }
-
-
-
         /// <summary>
         /// 这部分代码可能要重构
         /// </summary>
@@ -94,25 +147,23 @@ namespace 节能计算
                 FinalitemsTemplateelement.SetAttribute("Location", targetstr + "\\" + "FinalitemsTemplate.xml");
                 DevicesDBelement.SetAttribute("Location", targetstr + "\\" + "DevicesDB.xml");
                 DevicesCategoryelement.SetAttribute("Location", targetstr + "\\" + "DevicesCategory.xml");
-                doc.Save(targetstr + "\\" + "Pjt.xml");
-               
+                doc.Save(targetstr + "\\" + "Pjt.xml");               
             }
-            System.Windows.Input.Mouse.OverrideCursor = null;
-         
+            System.Windows.Input.Mouse.OverrideCursor = null;         
         }
-
+       
+        
         private void CopyDirectoryAndFiles(string des, DirectoryInfo srcDir)
         {
             if (!des.EndsWith("\\"))
             {
                 des += "\\";
             }
-          //  string desPath = des + srcDir.Name + "\\";
+            //  string desPath = des + srcDir.Name + "\\";
             //if (!Directory.Exists(desPath))
             //{
             //    Directory.CreateDirectory(desPath);
             //}
-
             foreach (FileInfo file in srcDir.GetFiles())
             {
                 file.CopyTo(des + file.Name, true);
@@ -123,10 +174,65 @@ namespace 节能计算
             }
         }
 
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            string location = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            string original = GlobalResource.Templatepath;
+            if (!string.IsNullOrEmpty(this.proname.Text) && !string.IsNullOrEmpty(this.prolocation.Text))
+            {
+                /*
+                 * *
+                 * 
+                 * 这里可以加一个正则，判断this.prolocation.Text是否是文件的正确路径
+                 * /
+                 * 
+                 */
+                if (string.IsNullOrEmpty(this.工程名称textbox.Text) || string.IsNullOrEmpty(this.设计单位textbox.Text) || string.IsNullOrEmpty(this.施工单位textbox.Text) || string.IsNullOrEmpty(this.养护技术textbox.Text))
+                {
+                    Xceed.Wpf.Toolkit.MessageBox.Show("请填写完整！");
+                    return;
+                }
+                //当两个字符串非空的时候，才能做 
+                var targetstr = this.prolocation.Text + "\\" + this.proname.Text;
+                if (!Directory.Exists(targetstr))
+                {
+                    // Create the directory it does not exist.
+                    Directory.CreateDirectory(targetstr);
+                    CopyDirectoryAndFiles(targetstr, new DirectoryInfo(location + original));
+                }
+                else
+                {
+                    Xceed.Wpf.Toolkit.MessageBox.Show("项目名已存在，请重新创建！");
+                    return;
+                }              
+                pjtxmllocation = targetstr + "\\" + "Pjt.xml";
+                XmlDocument doc = new XmlDocument();
+                doc.Load(targetstr + "\\" + "Pjt.xml");
+                var FinalitemsTemplateelement = doc.SelectSingleNode("Files/File[1]") as XmlElement;
+                var DevicesDBelement = doc.SelectSingleNode("Files/File[2]") as XmlElement;
+                var DevicesCategoryelement = doc.SelectSingleNode("Files/File[3]") as XmlElement;
+                FinalitemsTemplateelement.SetAttribute("Location", targetstr + "\\" + "FinalitemsTemplate.xml");
+                DevicesDBelement.SetAttribute("Location", targetstr + "\\" + "DevicesDB.xml");
+                DevicesCategoryelement.SetAttribute("Location", targetstr + "\\" + "DevicesCategory.xml");
+                doc.Save(targetstr + "\\" + "Pjt.xml");
+                var ConfigPathSelectedelement = _configdoc.SelectSingleNode("Config/ConfigItem[1]") as XmlElement;
+                ConfigPathSelectedelement.SetAttribute("Value",this.prolocation.Text);
+                _configdoc.Save("./Config.xml");
+                Button_Click_1(null, null);
+
+            }
+            else
+            {
+                Xceed.Wpf.Toolkit.MessageBox.Show("请输入项目！");
+            }
+            
+        }
+
+        
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
            // string location = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
-         //   string original = GlobalResource.Templatepath;
+           //   string original = GlobalResource.Templatepath;
             var proname = this.proname.Text;
          
             if (pjtxmllocation == null)
